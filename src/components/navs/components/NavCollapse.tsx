6377d3952573cb0746a49c8c3f55c40e458748f5
ma.kosicki <kosicki26@gmail.com>
2022-12-15 23:06:25 +0100

    add icons to sidebar

--- src/components/navs/components/NavCollapse.tsx
+++ src/components/navs/components/NavCollapse.tsx
@@ -1,4 +1,6 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { Collapse, Nav } from "react-bootstrap"
+import When from "../../utils/When"
 import CollapseButton from "./CollapseButton"
 
 type TProps = {
@@ -10,25 +12,30 @@ type TProps = {
 type TNavItem = {
     href?: string,
     title: string,
+    icon?: any,
     items?: TSubNavItem[]
 }
 
 type TSubNavItem = {
     href: string,
-    title: string
+    title: string,
+    icon?: any
 }
 
-function NavCollapse({isActive, navItem, changeCollapse}: TProps) {
+function NavCollapse({ isActive, navItem, changeCollapse }: TProps) {
     return (
         <>
             <Nav.Item onClick={changeCollapse}>
-                <CollapseButton title={navItem.title} active={isActive} />
+                <CollapseButton title={navItem.title} active={isActive} icon={navItem.icon} />
             </Nav.Item>
             <Collapse in={isActive}>
                 <div>
                     {navItem.items?.map((subItem, index) =>
                         <div className='ms-3' key={index}>
-                            <Nav.Link href={subItem.href}>{subItem.title}</Nav.Link>
+                            <Nav.Link href={subItem.href}>
+                                <When condition={subItem.icon !== undefined}><FontAwesomeIcon className="side-bar__nav-item-sub-item-icon" icon={subItem.icon} /></When>
+                                {subItem.title}
+                            </Nav.Link>
                         </div>
                     )}
                 </div>
--- src/components/navs/index.tsx
+++ src/components/navs/index.tsx
@@ -1,31 +1,40 @@
+import { faHome, faCarCrash, faCableCar, faCake, faEarDeaf } from "@fortawesome/free-solid-svg-icons"
+
 export const content = [
     {
         href: '/home',
-        title: 'Active'
+        title: 'Active',
+        icon: faHome
     },
     {
         title: 'collapse1',
+        icon: faCarCrash,
         items: [
             {
                 href: '/collapse1/col1',
-                title: 'Col1'
+                title: 'Col1',
+                icon: faCableCar
             },
             {
                 href: '/collapse1/col2',
-                title: 'Col2'
+                title: 'Col2',
+                icon: faCake
             }
         ]
     },
     {
         title: 'collapse2',
+        icon: faEarDeaf,
         items: [
             {
                 href: '/collapse2/col2',
-                title: 'Col21'
+                title: 'Col21',
+                icon: faCake
             },
             {
                 href: '/collapse2/col2',
-                title: 'Col22'
+                title: 'Col22',
+                icon: faEarDeaf
             }
         ]
     }
